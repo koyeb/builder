5361bd0b4a72e57a69040e1a6ff7d5d3e6c9cf45
Bastien Chatelard <chatel.bast@gmail.com>
2021-10-27 12:04:33 +0200

    Rework how builders are patched

--- patch.sh
+++ patch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-out=$(grep -c 'koyeb/custom' $1)
+function patch_koyeb_custom() {
+  echo "patching koyeb custom"
+  out=$(grep -c 'koyeb/custom' $1)
 
-if [[ "$out" -gt "1" ]]; then
-  echo "File '$1' already patched"
-  exit 0
-fi
+  if [[ "$out" -gt "1" ]]; then
+    echo "File '$1' already patched with custom"
+    return
+  fi
 
-load=$(cat $1 | yj -t)
-echo $load | jq '.buildpacks |= . + [{"id": "koyeb/custom", "uri": "docker://koyeb/custom-buildpack"}]' | jq '.order[].group |= . + [{"id": "koyeb/custom", "version": "0.1.0", "optional": true}]' | yj -jt -i > $1
+  load=$(cat $1 | yj -t)
+  echo $load | jq '.buildpacks |= . + [{"id": "koyeb/custom", "uri": "docker://koyeb/custom-buildpack"}]' | jq '.order[].group |= . + [{"id": "koyeb/custom", "version": "0.1.0", "optional": true}]' | yj -jt -i > $1
+}
+
+patch_koyeb_custom $1
